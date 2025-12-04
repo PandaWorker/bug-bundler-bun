caa815ef47b70f745f79e73e517ca3b387d61d05
Panda <you@example.com>
2025-12-04 14:02:55 +0300

    add interceptors log

--- src/bug.ts
+++ src/bug.ts
@@ -6,5 +6,8 @@ import B from "undici/index.js";
 console.log(A.Dispatcher, typeof new A.Dispatcher().dispatch); // undefined
 console.log(B.Dispatcher, typeof new B.Dispatcher().dispatch); // function
 
-console.log(Object.keys(A));
-console.log(Object.keys(B));
+console.log(Object.keys(A)); // 49 keys
+console.log(Object.keys(B)); // 55 keys
+
+console.log(A.interceptors);
+console.log(B.interceptors);
