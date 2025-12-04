b7f5fe870b7efa7d79fed6f238c5dcbddc8c7e97
Panda <you@example.com>
2025-12-04 14:05:18 +0300

    update

--- src/bug.ts
+++ src/bug.ts
@@ -3,11 +3,11 @@ import B from "undici/index.js";
 
 // console.log(A, B);
 
-console.log(A.Dispatcher, typeof new A.Dispatcher().dispatch); // undefined
-console.log(B.Dispatcher, typeof new B.Dispatcher().dispatch); // function
+console.log(`A`, A.Dispatcher, typeof new A.Dispatcher().dispatch); // undefined
+console.log(`B`, B.Dispatcher, typeof new B.Dispatcher().dispatch); // function
 
-console.log(Object.keys(A)); // 49 keys
-console.log(Object.keys(B)); // 55 keys
+console.log(`A`, Object.keys(A)); // 49 keys
+console.log(`B`, Object.keys(B)); // 55 keys
 
-console.log(A.interceptors);
-console.log(B.interceptors);
+console.log(`A`, A.interceptors);
+console.log(`B`, B.interceptors);
